e1eece1c759d417fe10b72ecd35894b32b9d2222
breno nogueira <breno.nog.araujo@hotmail.com>
2022-05-19 23:31:53 -0400

    connection with firebase and authController / routes

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,6 +9,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("HomePage"),
+        backgroundColor: AppColors.primary,
       ),
     );
   }
